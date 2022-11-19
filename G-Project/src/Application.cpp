dade97521758fbcc4d2fc30ce8e76d3af3f18e82
Nepentheee <564922212@qq.com>
2022-11-19 22:51:25 +0800

    commit glfw

--- G-Project/src/Application.cpp
+++ G-Project/src/Application.cpp
@@ -1,15 +1,40 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
+#include <GLAD/glad.h>
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+    cout << width << "," << height << endl;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    // OpenGL 3.3 Core_Profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(960, 640, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,10 +44,24 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Fail to initialize GLAD" << endl;
+        return -1;
+    }
+
+    // äÖÈ¾´°¿Ú´óÐ¡
+    glViewport(0, 0, 800, 640);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
         /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
@@ -30,6 +69,7 @@ int main(void)
         glVertex2f( 0.0f,  0.0f);
         glVertex2f( 0.5f, -0.5f);
         glEnd();
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
